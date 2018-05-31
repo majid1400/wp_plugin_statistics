2f6e28ea149fd176eb72220b26859fa7257d0e52
majid1400 <m.b1400v5@gmail.com>
2018-05-31 20:26:55 +0430

    add tpl folder & html code backend dashbord

--- inc/backend.php
+++ inc/backend.php
@@ -1,17 +1,25 @@
 <?php
-function wps_admin_menu(){
-    echo '<h1>آمار بازدیدکنندگان</h1>';
+function wps_admin_menu()
+{
+    global $wpdb, $table_prefix;
+
+    $statistics = $wpdb->get_row("SELECT SUM(total_visits) as total_visits, 
+                                               SUM(unique_visits) as total_unique_visits 
+                                        FROM {$table_prefix}wps_visits");
+
+    include WPS_TPL . 'admin_page_wps.php';
 }
 
-function wps_register_menu_page(){
-    add_menu_page( 
+function wps_register_menu_page()
+{
+    add_menu_page(
         'آمار بازدید کنندگان',
         'آمار بازدید کنندگان',
         'manage_options',
         'wps/wps-stat.php',
         'wps_admin_menu',
-        'dashicons-chart-area', 
-        6 );
+        'dashicons-chart-area',
+        6);
 }
 
-add_action( 'admin_menu', 'wps_register_menu_page');
+add_action('admin_menu', 'wps_register_menu_page');
