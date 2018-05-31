2b85bda8324137407504a2e03c7e3861bab8fcce
majid1400 <m.b1400v5@gmail.com>
2018-05-31 20:27:27 +0430

    add tpl folder & html code backend dashbord

---
+++ tpl/admin_page_wps.php
@@ -0,0 +1,24 @@
+<div class="wrap">
+    <h1>آمار بازدید کنندگان</h1>
+    <div id="dashboard-widgets" class="metabox-holder">
+        <div id="postbox-container-1" class="postbox-container">
+            <div id="normal-sortables" class="meta-box-sortables ui-sortable">
+                <div id="dashboard_right_now" class="postbox">
+                    <h2 class="hndle ui-sortable-handle"><span>خلاصه آمار بازدید</span></h2>
+                    <div class="inside">
+                        <div class="main">
+                            <p>
+                                <span>بازدید کل: </span>
+                                <span><?php echo $statistics->total_visits ?></span>
+                            </p>
+                            <p>
+                                <span>بازدید یکتای کل: </span>
+                                <span><?php echo $statistics->total_unique_visits ?></span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
