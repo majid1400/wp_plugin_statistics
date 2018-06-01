c0891dd80479fb206cf88cec8aeb0fb31529ca76
majid1400 <m.b1400v5@gmail.com>
2018-06-01 14:44:08 +0430

    add date today & yesterday & month & Chart.min.js

--- inc/backend.php
+++ inc/backend.php
@@ -2,12 +2,26 @@
 function wps_admin_menu()
 {
     global $wpdb, $table_prefix;
+    $today = date("Y-m-d");
 
-    $statistics = $wpdb->get_row("SELECT SUM(total_visits) as total_visits, 
+    $totalStatistics = $wpdb->get_row("SELECT SUM(total_visits) as total_visits, 
                                                SUM(unique_visits) as total_unique_visits 
                                         FROM {$table_prefix}wps_visits");
 
+    $todayStatistics = $wpdb->get_row("SELECT total_visits, unique_visits 
+                                             FROM {$table_prefix}wps_visits
+                                             WHERE date='{$today}' ");
+
+    $yesterdayStatistics = $wpdb->get_row("SELECT total_visits, unique_visits 
+                                             FROM {$table_prefix}wps_visits
+                                             WHERE date >= DATE_SUB('{$today}', INTERVAL 1 DAY)");
+
+    $monthStatistics = $wpdb->get_row("SELECT total_visits, unique_visits 
+                                             FROM {$table_prefix}wps_visits
+                                             WHERE date >= DATE_SUB('{$today}', INTERVAL DAYOFMONTH('{$today}')-1 DAY)");
+
     include WPS_TPL . 'admin_page_wps.php';
+    wp_load_assets();
 }
 
 function wps_register_menu_page()
@@ -16,10 +30,16 @@ function wps_register_menu_page()
         'آمار بازدید کنندگان',
         'آمار بازدید کنندگان',
         'manage_options',
-        'wps/wps-stat.php',
+        'wps-stat.php',
         'wps_admin_menu',
         'dashicons-chart-area',
         6);
 }
 
 add_action('admin_menu', 'wps_register_menu_page');
+
+function wp_load_assets()
+{
+    wp_register_script('Chart.min.js', WPS_JS . 'Chart.min.js', array('jquery'));
+    wp_enqueue_script('Chart.min.js');
+}
--- tpl/admin_page_wps.php
+++ tpl/admin_page_wps.php
@@ -9,12 +9,52 @@
                         <div class="main">
                             <p>
                                 <span>بازدید کل: </span>
-                                <span><?php echo $statistics->total_visits ?></span>
+                                <span><?php echo $totalStatistics->total_visits ?></span>
                             </p>
                             <p>
                                 <span>بازدید یکتای کل: </span>
-                                <span><?php echo $statistics->total_unique_visits ?></span>
+                                <span><?php echo $totalStatistics->total_unique_visits ?></span>
                             </p>
+                            <hr>
+
+                            <p>
+                                <span>بازدید کل امروز: </span>
+                                <span><?php echo intval($todayStatistics->total_visits) ?></span>
+                            </p>
+                            <p>
+                                <span>بازدید یکتای امروز: </span>
+                                <span><?php echo intval($todayStatistics->unique_visits) ?></span>
+                            </p>
+                            <hr>
+
+                            <p>
+                                <span>بازدید کل دیروز: </span>
+                                <span><?php echo intval($yesterdayStatistics->total_visits) ?></span>
+                            </p>
+                            <p>
+                                <span>بازدید یکتای دیروز: </span>
+                                <span><?php echo intval($yesterdayStatistics->unique_visits) ?></span>
+                            </p>
+                            <hr>
+
+                            <p>
+                                <span>بازدید کل ماهانه: </span>
+                                <span><?php echo intval($monthStatistics->total_visits) ?></span>
+                            </p>
+                            <p>
+                                <span>بازدید یکتای ماهانه: </span>
+                                <span><?php echo intval($monthStatistics->unique_visits) ?></span>
+                            </p>
+
+
+
+<!--                            <form action="#" method="get">-->
+<!--                                تاریخ دلخواه وارد کنید:-->
+<!--                                <input type="date" name="bday">-->
+<!--                                <input type="submit" name="ali">-->
+<!--                            </form>-->
+<!--                            --><?php //var_dump($_GET['name']); ?>
+
                         </div>
                     </div>
                 </div>
